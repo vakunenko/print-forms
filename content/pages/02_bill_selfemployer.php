f9639d50272f58f46c245007b59a514eaee652f3
Pavel Vakunenko <vakunenko@mail.ru>
2020-10-25 14:23:17 +0300

    Load project

---
+++ content/pages/02_bill_selfemployer.php
@@ -0,0 +1,179 @@
+<?php
+    $pdf = '
+    <div class="print">
+        <table>
+            <tr>
+                <td class="small">Внимание! Оплата данного счёта означает согласие с условиями поставки товара или оказания услуг. Уведомление об оплате обязательно, в противном случае не гарантируется наличие товара на складе. Товар отпускается по факту прихода денег на р/с Поставщика.</td>
+            </tr>
+
+            <tr>
+                <td class="divider"></td>
+            </tr>
+
+            <tr>
+                <td class="small">Образец заполнения платежного поручения.</td>
+            </tr>
+
+            <tr>
+                <td class="divider"></td>
+            </tr>
+        </table>
+
+        <table class="border">
+            <tr class="layout">
+                <td width="10%"></td>
+                <td width="20%"></td>
+                <td width="10%"></td>
+                <td width="20%"></td>
+                <td width="10%"></td>
+                <td width="30%"></td>
+            </tr>
+
+            <tr>
+                <td rowspan="2" colspan="4">
+                    <span class="data">XXXX</span>
+                    <br /><br /><span class="small">Банк получателя</span>
+                </td>
+                <td>БИК</td>
+                <td><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td>Сч. №</td>
+                <td><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td>ИНН</td>
+                <td><span class="data">XXXX</span></td>
+                <td colspan="2"></td>
+                <td rowspan="2">Сч. №</td>
+                <td rowspan="2"><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td colspan="4"><span class="data">XXXX (самозанятый)</span>
+                <br /><br /><span class="small">Получатель</span></td>
+            </tr>
+        </table>
+
+        <br />
+
+        <table>
+            <tr>
+                <td class="text-center h1">Счёт на оплату №<span class="data">XXXX</span> от <span class="data">XXXX</span> г.</td>
+            </tr>
+        </table>
+
+        <br />
+
+        <table>
+            <tr class="layout">
+                <td width="15%"></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td class="text-top">Покупатель:</td>
+                <td><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td colspan="2" class="divider"></td>
+            </tr>
+
+            <tr>
+                <td class="text-top">Поставщик:</td>
+                <td><span class="data">XXXX</span></td>
+            </tr>
+        </table>
+
+        <br />
+
+        <table class="border">
+            <tr class="layout">
+                <td width="5%"></td>
+                <td></td>
+                <td width="10%"></td>
+                <td width="10%"></td>
+                <td width="12%"></td>
+                <td width="12%"></td>
+            </tr>
+
+            <tr class="text-center">
+                <th>№</th>
+                <th>Наименование товара (услуги)</th>
+                <th>Кол-во</th>
+                <th>Ед.</th>
+                <th>Цена</th>
+                <th>Сумма</th>
+            </tr>
+
+            <tr>
+                <td class="text-center">1</td>
+                <td><span class="data">XXXX</span></td>
+                <td><span class="data">XXXX</span></td>
+                <td><span class="data">XXXX</span></td>
+                <td><span class="data">XXXX</span></td>
+                <td><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td colspan="5" class="no-border text-right b">Итого:</td>
+                <td class="b"><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td colspan="5" class="no-border text-right b">В том числе НДС:</td>
+                <td class="b"><span class="data">XXXX</span></td>
+            </tr>
+
+            <tr>
+                <td colspan="5" class="no-border text-right b">Всего к оплате:</td>
+                <td class="b"><span class="data">XXXX</span></td>
+            </tr>
+        </table>
+
+        <br />
+
+        <table>
+            <tr>
+                <td>Всего наименований: <span class="data">XXXX</span>, на сумму <span class="data">XXXX</span> руб.</td>
+            </tr>
+
+            <tr>
+                <td class="b"><span class="data">XXXX</span></td>
+            </tr>
+        </table>
+
+        <br />
+
+        <table>
+            <tr class="layout">
+                <td></td>
+                <td width="20%"></td>
+                <td width="1%"></td>
+                <td width="30%"></td>
+            </tr>
+
+            <tr>
+                <td>Самозанятый</td>
+                <td></td>
+                <td></td>
+                <td class="text-center"></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td class="sign">(подпись)</td>
+                <td></td>
+                <td class="sign">(расшифровка подписи)</td>
+            </tr>
+        </table>
+    </div>
+    ';
+
+    $content = str_replace('{$pdf}', $pdf, $html);
+
+    $mpdf->AddPage('P');
+    $mpdf->WriteHTML($content, 2);
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    require_once ('vendor/autoload.php');
+
+    $mpdf = new \Mpdf\Mpdf([
+        'mode'          => 'utf-8',
+        'format'        => 'A4',
+        'default_font'  => 'sans-serif',
+        'margin_left'   => 12,
+        'margin_right'  => 12,
+        'margin_top'    => 10,
+        'margin_bottom' => 10,
+    ]);
+
+    $css  = file_get_contents('framework/styles/css/pdf.min.css');
+    $html = file_get_contents('content/templates/pdf.html');
+
+    $mpdf->WriteHTML($css, 1);
+
+    require_once ('content/pages/01_bill.php');              // Счёт
+    require_once ('content/pages/02_bill_selfemployer.php'); // Счёт (самозанятый)
+
+    $mpdf->Output('print.pdf', 'I');
